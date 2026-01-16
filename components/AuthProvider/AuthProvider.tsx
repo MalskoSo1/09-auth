641bc3e9c10820f58d20206f5f0f354260c95c73
Ліза Гоцко <filani9818@gmail.com>
2026-01-16 19:40:58 +0200

    23

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -4,15 +4,22 @@ import { checkSession, getMe } from "@/lib/api/clientApi";
 import { useAuthStore } from "@/lib/store/authStore";
 import { useEffect, useState } from "react";
 import Loader from "../Loader/Loader";
+import { usePathname, useRouter } from "next/navigation";
+
+const privateRoutes = ["/notes", "/profile"];
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const setUser = useAuthStore((s) => s.setUser);
   const clearUser = useAuthStore((s) => s.clearUser);
   const [loading, setLoading] = useState(true);
 
+  const router = useRouter();
+  const pathname = usePathname();
+
   useEffect(() => {
     const fetchUser = async () => {
       const isAuthorized = await checkSession();
+
       if (isAuthorized) {
         const user = await getMe();
         if (user) {
@@ -20,12 +27,15 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         }
       } else {
         clearUser();
+        if (privateRoutes.some((path) => pathname.startsWith(path))) {
+          router.replace("/sign-in");
+        }
       }
       setLoading(false);
     };
 
     fetchUser();
-  }, []);
+  }, [pathname, router, setUser, clearUser]);
 
   if (loading) return <Loader />;
   return children;
